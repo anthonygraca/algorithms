53cb16a10f125799c6793fee6dba3f7c7dac8b26
anthonygraca <anthonygraca@gmail.com>
2021-05-29 15:07:12 -0700

    first commit

---
+++ src/main/java/io/anthonygraca/App.java
@@ -0,0 +1,13 @@
+package io.anthonygraca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
