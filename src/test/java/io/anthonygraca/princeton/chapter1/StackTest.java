c90d577c76a1938e65ace366aa3d18fcbd8dbfac
anthonygraca <anthonygraca@gmail.com>
2021-05-31 16:45:42 -0700

    added blank test and source for stack

---
+++ src/test/java/io/anthonygraca/princeton/chapter1/StackTest.java
@@ -0,0 +1,13 @@
+package io.anthonygraca.princeton.chapter1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class StackTest {
+  @Test
+  public void shouldAnswerWithTrue() {
+    Stack stack = new Stack();
+    assertTrue( true );
+  }
+}
