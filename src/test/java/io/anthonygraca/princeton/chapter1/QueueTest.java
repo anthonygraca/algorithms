99fd420b641ea2426a72018e32f38dabcd9b8ba4
matthewgraca <mgraca@cpp.edu>
2021-06-02 14:42:37 -0700

    minor formatting changes, fixed withinBoundConstructor() to actually test the queue capacities, and added test declarations for the enqueueing methods

--- src/test/java/io/anthonygraca/princeton/chapter1/QueueTest.java
+++ src/test/java/io/anthonygraca/princeton/chapter1/QueueTest.java
@@ -5,14 +5,17 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class QueueTest {
+  /*
+   * These test the functionality of the constructors
+   */
   @Test
-  public void shouldAnswerWithTrue() {
+  public void defaultConstructor() {
     Queue queue = new Queue();
     assertTrue( true );
   }
   
   @Test
-  public void lowerBoundConstructorTest(){
+  public void lowerBoundConstructor(){
     try{
       Queue queue = new Queue(-1);
     }
@@ -22,7 +25,7 @@ public class QueueTest {
   }
 
   @Test
-  public void upperBoundConstructorTest(){
+  public void upperBoundConstructor(){
     try{
       Queue queue = new Queue(100000);
     }
@@ -32,11 +35,37 @@ public class QueueTest {
   }
 
   @Test
-  public void withinBoundConstructorTest(){
-    Queue queue1 = new Queue(1);
-    Queue queue2 = new Queue(10);
-    Queue queue3 = new Queue(100);
-    Queue queue4 = new Queue(1000);
-    Queue queue5 = new Queue(10000);
+  public void withinBoundConstructor(){
+    try{
+      Queue queue1 = new Queue(1);      // exact lower bound
+      Queue queue2 = new Queue(10);
+      Queue queue3 = new Queue(100);
+      Queue queue4 = new Queue(1000);
+      Queue queue5 = new Queue(10000);  // exact upper bound
+    }
+    catch(Exception e){
+      assertTrue(false);
+    }
+    assertTrue(true);
+  }
+
+  /*
+   * These test the functionality of the enqueueing method
+   */
+  @Test
+  public void addedToEmptyQueue(){
+    assertTrue(true);
+  }
+
+  @Test
+  public void addedToFullQueue(){
+
+    assertTrue(true);
+  }
+
+  @Test
+  public void addedToHalfFullQueue(){
+
+    assertTrue(true);
   }
 }
