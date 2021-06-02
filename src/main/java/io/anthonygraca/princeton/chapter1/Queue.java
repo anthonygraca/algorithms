e7dc387e15a7ec2077bc14e039b69dcc36ecc300
matthewgraca <mgraca@cpp.edu>
2021-06-02 14:39:11 -0700

    changed +1 to + OFFSET in constructor for clarity. Added method declarations

--- src/main/java/io/anthonygraca/princeton/chapter1/Queue.java
+++ src/main/java/io/anthonygraca/princeton/chapter1/Queue.java
@@ -9,23 +9,31 @@ public class Queue<T> {
   private int back;
   private static final int DEFAULT_CAPACITY = 25;
   private static final int MAX_CAPACITY = 10000;
+  private static final int OFFSET = 1;  // allows front/back index tracking with modulus
 
+  /**
+   * Default constructor for Queue
+   */
   public Queue(){
     this(DEFAULT_CAPACITY);
   }
 
+  /**
+   * Constructor that creates a queue based on the given capacity
+   * @param capacity  the capacity of the queue being created
+   */
   public Queue(int capacity){
     checkCapacity(capacity);
 
     // java garbage to enable usage of generic arrays
     @SuppressWarnings("unchecked")
-    T[] tempQueue = (T[])new Object[capacity + 1];
+    T[] tempQueue = (T[])new Object[capacity + OFFSET];
     queue = tempQueue;
     front = 0;
     back = capacity;
   }
 
-  /*
+  /**
    * Ensures the defined capacity of the queue is within the proper boundaries
    */
   private void checkCapacity(int capacity){
@@ -35,5 +43,52 @@ public class Queue<T> {
     else if (capacity <= 0){
       throw new IllegalStateException("Queue capacity must be greater than 0");
     }
+  }
+
+  /**
+   * Adds an item to the queue
+   * @param item object to be placed into the queue
+   */
+  public void enqueue(T item){
+
+  }
+
+  /**
+   * Removes an item from the beginning of the queue
+   * @return The item that was removed from the queue
+   */
+  public T dequeue(){
+    return null;
+  }
+
+  /**
+   * Checks the front of the queue
+   * @return the object at the front of the queue
+   */
+  public T peek(){
+    return null;
+  }
+
+  /**
+   * Checks if the queue is empty
+   * @return True if the queue is empty, false if not
+   */
+  public boolean isEmpty(){
+    return false;
+  }
+
+  /**
+   * Checks if the queue is full
+   * @return True if the queue is full, false if not
+   */
+  public boolean isFull(){
+    return false;
+  }
+
+  /**
+   * Empties the contents of the queue
+   */
+  public void clear(){
+
   }
 }
