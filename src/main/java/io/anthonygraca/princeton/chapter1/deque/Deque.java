7247b070c6465aecdfe1630189cd49c6601e905f
anthonygraca <anthonygraca@gmail.com>
2021-06-02 20:39:20 -0700

    added trivial addFirst(), addLast(), and removeFirst()

--- src/main/java/io/anthonygraca/princeton/chapter1/deque/Deque.java
+++ src/main/java/io/anthonygraca/princeton/chapter1/deque/Deque.java
@@ -1,6 +1,34 @@
 package io.anthonygraca.princeton.chapter1;
 
-class Deque {
+import java.util.NoSuchElementException;
+
+public class Deque<Item> {
+
+  private int mSize = 0;
+
+  public int size() {
+    return mSize;
+  }
+
+  public boolean isEmpty() {
+    return mSize == 0;
+  }
+
+  public void addFirst(Item item) {
+    mSize++;
+  }
+
+  public void addLast(Item item) {
+    mSize++;
+  }
+
+  public Item removeFirst() {
+    if (isEmpty()) {
+      throw new NoSuchElementException("Deque is empty. Cannot remove");
+    }
+    mSize--;
+    return (Item) new Object();
+  }
 
 }
 
--- src/test/java/io/anthonygraca/princeton/chapter1/deque/DequeTest.java
+++ src/test/java/io/anthonygraca/princeton/chapter1/deque/DequeTest.java
@@ -1,13 +1,57 @@
 package io.anthonygraca.princeton.chapter1;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import java.util.NoSuchElementException;
 
 import org.junit.Test;
 
 public class DequeTest {
   @Test
-  public void shouldAnswerWithTrue() {
+  public void constructEmptyDeque() {
     Deque deque = new Deque();
-    assertTrue( true );
+    assertTrue(deque.size() == 0);
   }
+
+  @Test
+  public void verifyNewDequeIsEmpty() {
+    Deque deque = new Deque();
+    assertTrue(deque.isEmpty());
+  }
+
+  @Test
+  public void addFirstItem() {
+    Deque deque = new Deque();
+    Item item = new Item();
+    deque.addFirst(item);
+    assertFalse(deque.isEmpty());
+  }
+
+  @Test
+  public void addLastItem() {
+    Deque deque = new Deque();
+    Item item = new Item();
+    deque.addLast(item);
+    assertFalse(deque.isEmpty());
+  }
+
+  @Test
+  public void removeFirstItem() {
+    Deque deque = new Deque();
+    Item item = new Item();
+    deque.addFirst(item);
+    deque.removeFirst();
+    assertTrue(deque.isEmpty());
+  }
+
+  @Test(expected=NoSuchElementException.class)
+  public void removeFirstItemWithoutAdd() {
+    Deque deque = new Deque();
+    Item item = new Item();
+    deque.removeFirst();
+  }
+
+  public class Item {}
 }
+
