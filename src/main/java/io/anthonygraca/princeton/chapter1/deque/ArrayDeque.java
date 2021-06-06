200b6bd1c2d8d5626cabcc9857da7550ab85f2ff
matthewgraca <mgraca@cpp.edu>
2021-06-06 14:10:16 -0700

    added constrcutors and checkCapacity,  tested constructors

--- src/main/java/io/anthonygraca/princeton/chapter1/deque/ArrayDeque.java
+++ src/main/java/io/anthonygraca/princeton/chapter1/deque/ArrayDeque.java
@@ -3,7 +3,49 @@ package io.anthonygraca.princeton.chapter1;
 import java.util.NoSuchElementException;
 
 public class ArrayDeque<T> {
-  private int size = 0;
+  private T[] deque;
+  private int front;
+  private int back;
+  private static final int DEFAULT_CAPACITY = 25;
+  private static final int MAX_CAPACITY = 10000;
+  private int size;
+  private final int OFFSET = 1;
+
+  /**
+   * Default constructor for deque
+   */
+  public ArrayDeque(){
+    this(DEFAULT_CAPACITY);
+  }
+
+  /**
+   * Constructor that creates a deque based on a given capacity
+   * @param capacity  the capacity of the deque being created
+   */
+  public ArrayDeque(int capacity){
+    checkCapacity(capacity);
+
+    // java garbage to enable usage of generic arrays
+    @SuppressWarnings("unchecked")
+    T[] tempDeque = (T[])new Object[capacity + OFFSET];
+    deque = tempDeque;
+    front = 0;
+    back = front + 1;
+    size = capacity;
+  }
+
+  /**
+   * Checks if the capacity value is valid
+   * @param capacity  the capacity being checked
+   */
+  private void checkCapacity(int capacity){
+    if (capacity > MAX_CAPACITY){
+      throw new IllegalStateException("Deque capacity exceeds the max capacity of " + MAX_CAPACITY);
+    }
+    else if (capacity <= 0){
+      throw new IllegalStateException("Deque capacity must be greater than 0");
+    }
+  }
 
   /**
    * Returns the size of the deque
--- src/test/java/io/anthonygraca/princeton/chapter1/deque/ArrayDequeTest.java
+++ src/test/java/io/anthonygraca/princeton/chapter1/deque/ArrayDequeTest.java
@@ -8,10 +8,36 @@ import java.util.NoSuchElementException;
 import org.junit.Test;
 
 public class ArrayDequeTest {
+  // Testing constructors
   @Test
-  public void constructEmptyDeque(){
+  public void constructDefaultDeque(){
     ArrayDeque deque = new ArrayDeque();
-    assertTrue(deque.size() == 0);
+    assertTrue(deque.size() == 25);
+  }
+
+  @Test(expected=IllegalStateException.class)
+  public void lowerBoundConstructor(){
+    ArrayDeque queue = new ArrayDeque(-1);
+  }
+
+  @Test(expected=IllegalStateException.class)
+  public void upperBoundConstructor(){
+    ArrayDeque queue = new ArrayDeque(100000);
+  }
+
+  @Test
+  public void withinBoundConstructor(){
+    try{
+      ArrayDeque queue1 = new ArrayDeque(1);      // exact lower bound
+      ArrayDeque queue2 = new ArrayDeque(10);
+      ArrayDeque queue3 = new ArrayDeque(100);
+      ArrayDeque queue4 = new ArrayDeque(1000);
+      ArrayDeque queue5 = new ArrayDeque(10000);  // exact upper bound
+    }
+    catch(Exception e){
+      assertTrue(false);
+    }
+    assertTrue(true);
   }
 }
 
