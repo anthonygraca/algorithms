42f3be080ec652d94f8f45b6fc597ad6572c398e
anthonygraca <anthonygraca@gmail.com>
2021-06-08 22:38:00 -0700

    added simple FixedCapacityStackOfStrings and Test

---
+++ src/main/java/io/anthonygraca/princeton/chapter1/stack/FixedCapacityStackOfStrings.java
@@ -0,0 +1,11 @@
+package io.anthonygraca.princeton.chapter1.stack;
+
+public class FixedCapacityStackOfStrings {
+  void push(String string) {
+
+  }
+
+  boolean isEmpty() {
+    return false;
+  }
+}
---
+++ src/test/java/io/anthonygraca/princeton/chapter1/stack/FixedCapacityStackOfStringsTest.java
@@ -0,0 +1,16 @@
+package io.anthonygraca.princeton.chapter1.stack;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class FixedCapacityStackOfStringsTest {
+  @Test
+  public void pushAString() {
+    FixedCapacityStackOfStrings stack = new FixedCapacityStackOfStrings();
+    String example = "dummy string";
+    stack.push(example);
+    assertFalse(stack.isEmpty());
+  }
+}
