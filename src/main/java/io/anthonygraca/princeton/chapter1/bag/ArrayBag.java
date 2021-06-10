7ba8238c365f8b0d6e4c7803fa1fb91df9142437
matthewgraca <mgraca@cpp.edu>
2021-06-09 17:07:09 -0700

    implemented ArrayBag and corresponding tests

--- src/main/java/io/anthonygraca/princeton/chapter1/bag/ArrayBag.java
+++ src/main/java/io/anthonygraca/princeton/chapter1/bag/ArrayBag.java
@@ -1,6 +1,9 @@
 package io.anthonygraca.princeton.chapter1;
 
-public class ArrayBag<T> implements Bag<T>{
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ArrayBag<T> implements Bag<T>, Iterable<T>{
   private T[] bag;
   private int size;
   private final int DEFAULT_CAPACITY = 25;
@@ -9,7 +12,10 @@ public class ArrayBag<T> implements Bag<T>{
    * Default constructor
    */
   public ArrayBag(){
-
+    @SuppressWarnings("unchecked")
+    T[] tempBag = (T[])new Object[DEFAULT_CAPACITY];
+    bag = tempBag;
+    size = 0;
   }
 
   /**
@@ -17,7 +23,22 @@ public class ArrayBag<T> implements Bag<T>{
    * @param item  the item being added
    */
   public void add(T item){
+    if (isFull()){
+      doubleCapacity();
+    }
+    bag[size++] = item;
+  }
 
+  /**
+   * Doubles the capacity of the queue
+   */
+  private void doubleCapacity(){
+    @SuppressWarnings("unchecked")
+    T[] tempBag = (T[])new Object[2*size];
+    for (int i = 0; i < size; i++){
+      tempBag[i] = bag[i];
+    }
+    bag = tempBag;
   }
 
   /**
@@ -25,7 +46,29 @@ public class ArrayBag<T> implements Bag<T>{
    * @return  true if empty, false if not
    */
   public boolean isEmpty(){
-    return true;
+    return size == 0;
+  }
+
+  /**
+   * Returns the item at the top of the bag
+   * @return the item at the top of the bag
+   * @throws NoSuchElementException if the bag is empty
+   */
+  public T getItem(){
+    if (isEmpty()){
+      throw new NoSuchElementException();
+    }
+    else{
+      return bag[size - 1];
+    }
+  }
+
+  /**
+   * Checks if the bag is full
+   * @return true if full, false if not
+   */
+  private boolean isFull(){
+    return size == bag.length;
   }
 
   /**
@@ -33,6 +76,46 @@ public class ArrayBag<T> implements Bag<T>{
    * @return  the number of items currently in the bag
    */
   public int size(){
-    return 0;
+    return size;
+  }
+
+  /**
+   * Returns an iterator that iterates over the items in the bag arbitrarily
+   * @return an iterator that iterates over the items in the bag arbitrarily
+   */
+  public Iterator<T> iterator(){
+    return new ArrayIterator();
+  }
+
+  private class ArrayIterator implements Iterator<T>{
+    private int i = 0;
+
+    /**
+     * Detects if this iterator has gone beyond the last entry in the collection
+     * @return true if the iterator has another entry to return, false if not
+     */
+    public boolean hasNext(){
+      return i < size;
+    }
+
+    /**
+     * Removes from the list the last entry that either next() or previous() has returned
+     * @throws  UnsupportedOperationException if the iterator does not permit a remove operation
+     */
+    public void remove(){
+      throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Retrieves the next entry in the list and advances the iterator by one position
+     * @return a reference to the next entry in the iteration
+     * @throws NoSuchElementException if the iterator is at the end (hasNext() is false)
+     */
+    public T next(){
+      if (!hasNext()){
+        throw new NoSuchElementException();
+      }
+      return bag[i++];
+    }
   }
 }
--- src/test/java/io/anthonygraca/princeton/chapter1/BagTest.java
+++ src/test/java/io/anthonygraca/princeton/chapter1/BagTest.java
@@ -5,10 +5,78 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class BagTest {
+  /*
+   * These test the functionality of the constructor
+   */
   @Test
-  public void shouldAnswerWithTrue() {
-    Bag aBag = new ArrayBag();
-    Bag lBag = new LinkedBag();
-    assertTrue( true );
+  public void defaultConstructor(){
+    ArrayBag bag = new ArrayBag();
+    assertTrue(bag.size() == 0);
+  }
+
+  /*
+   * These test the functionality of size
+   */
+  @Test
+  public void sizeIncrements(){
+    ArrayBag<String> bag = new ArrayBag<>();
+    bag.add("some string");
+    assertTrue(bag.size() == 1);
+  }
+
+  @Test
+  public void sizeIncrementsThroughResizing(){
+    ArrayBag<String> bag = new ArrayBag<>();
+    for (int i = 0; i < 26; i++){
+      bag.add("String " + i);
+    }
+    assertTrue(bag.size() == 26);
+  }
+
+  /*
+   * These test the functionality of the add method
+   */
+  @Test
+  public void addedItemIsInBag(){
+    ArrayBag<String> bag = new ArrayBag<>();
+    bag.add("A string");
+    assertTrue(bag.getItem().equals("A string"));
+  }
+
+  @Test
+  public void bagProperlyResizesAndAdds(){
+    ArrayBag<String> bag = new ArrayBag<>();
+    for (int i = 0; i < 25; i++){
+      bag.add("String " + i);
+    }
+    bag.add("New string");
+    assertTrue(bag.getItem().equals("New string"));
+  }
+
+  /*
+   * These test the iterator
+   */
+  @Test
+  public void iteratorWorksWithoutResize(){
+    ArrayBag<String> bag = new ArrayBag<>();
+    for (int i = 0; i < 25; i++){
+      bag.add("A string");
+    }
+
+    for (String s : bag){
+      assertTrue(s.equals("A string"));
+    }
+  }
+
+  @Test
+  public void iteratorWorksWithResizing(){
+    ArrayBag<String> bag = new ArrayBag<>();
+    for (int i = 0; i < 51; i++){
+      bag.add("A string");
+    }
+
+    for (String s : bag){
+      assertTrue(s.equals("A string"));
+    }
   }
 }
