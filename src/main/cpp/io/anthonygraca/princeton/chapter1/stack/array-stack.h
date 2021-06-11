3ff4859db1152ec4de59fff228d9db140125f074
anthonygraca <anthonygraca@gmail.com>
2021-06-11 12:24:42 -0700

    commented out iterator test. applied clang-format

--- src/main/cpp/io/anthonygraca/princeton/chapter1/stack/array-stack.h
+++ src/main/cpp/io/anthonygraca/princeton/chapter1/stack/array-stack.h
@@ -4,38 +4,40 @@
 #include <utility>
 
 namespace algorithms {
-template<typename T>
+template <typename T>
 class ArrayStack {
-  public:
-    ArrayStack() : m_size{0}, m_capacity{1}, m_collection{new T[m_capacity]}{};
-    ~ArrayStack() {delete[] m_collection;};
-    bool isEmpty() {
-      return m_size == 0;
+public:
+  ArrayStack() : m_size{0}, m_capacity{1}, m_collection{new T[m_capacity]} {};
+  ~ArrayStack() { delete[] m_collection; };
+  bool isEmpty() {
+    return m_size == 0;
+  }
+  void push(T item) {
+    if (m_size == m_capacity)
+      resize(2 * m_capacity);
+    m_collection[m_size++] = item;
+  }
+  int getSize() { return m_size; }
+  int getCapacity() { return m_capacity; }
+  T pop() {
+    T item = std::move(m_collection[--m_size]);
+    if (m_size > 0 && m_size == m_capacity / 4)
+      resize(m_capacity / 2);
+    return std::move(item);
+  }
+private:
+  int m_size = -1;
+  int m_capacity = -1;
+  T *m_collection;
+  void resize(int max) {
+    m_capacity = max;
+    T *new_array = new T[m_capacity];
+    for (int i = 0; i < m_size; i++) {
+      new_array[i] = std::move(m_collection[i]);
     }
-    void push(T item) {
-      if (m_size == m_capacity) resize(2 * m_capacity);
-      m_collection[m_size++] = item;
-    }
-    int getSize() { return m_size; }
-    int getCapacity() { return m_capacity; }
-    T pop() {
-      T item = std::move(m_collection[--m_size]);
-      if (m_size > 0 && m_size == m_capacity/4) resize(m_capacity/2);
-      return std::move(item);
-    }
-  private:
-    int m_size = -1;
-    int m_capacity = -1;
-    T* m_collection;
-    void resize(int max) {
-      m_capacity = max;
-      T* new_array = new T[m_capacity];
-      for (int i = 0; i < m_size; i++) {
-        new_array[i] = std::move(m_collection[i]);
-      }
-      delete[] m_collection;
-      m_collection = new_array;
-    }
+    delete[] m_collection;
+    m_collection = new_array;
+  }
 };
 } // namespace algorithms
 #endif
--- src/test/cpp/io/anthonygraca/princeton/chapter1/stack/array-stack-test.cpp
+++ src/test/cpp/io/anthonygraca/princeton/chapter1/stack/array-stack-test.cpp
@@ -78,6 +78,21 @@ TEST(ArrayStack, VerifyTemplateType) {
   ASSERT_TRUE(example == stack.pop());
 }
 
+/*
+TEST(ArrayStack, VerifyIterator) {
+  algorithms::ArrayStack<std::string> stack;
+  int count = 0;
+  std::string example1 = "dummy string 1";
+  std::string example2 = "dummy string 2";
+  std::string example3 = "dummy string 3";
+  std::string example4 = "dummy string 4";
+  for (auto c : stack) {
+    count++;
+  }
+  ASSERT_TRUE(count == stack.getSize());
+}
+*/
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
