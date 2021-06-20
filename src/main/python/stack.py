74cb028f89041e6e33f8401d94070b71e066064e
anthonygraca <anthonygraca@gmail.com>
2021-06-20 06:29:57 -0700

    implemented stack in python

--- src/main/python/stack.py
+++ src/main/python/stack.py
@@ -1,3 +1,19 @@
 class stack:
+    def __init__(self):
+        self.m_size = 0
+        self.m_collection = []
+
     def isEmpty(self):
-        return True
+        return self.m_size == 0
+
+    def push(self, item):
+        self.m_collection.append(item)
+        self.m_size+=1
+
+    def getSize(self):
+        return self.m_size
+
+    def pop(self):
+        self.m_size-=1
+        return self.m_collection.pop(self.m_size)
+
--- src/test/python/stack_tests.py
+++ src/test/python/stack_tests.py
@@ -6,3 +6,60 @@ class StackTest(unittest.TestCase):
     def test_initialized_stack_is_empty(self):
         my_stack = stack()
         assert my_stack.isEmpty() == True
+
+    def test_push_a_string(self):
+        my_stack = stack()
+        item = "dummy string"
+        my_stack.push(item)
+        assert my_stack.isEmpty() == False
+
+    def test_verify_size(self):
+        my_stack = stack()
+        item = "dummy string"
+        my_stack.push(item)
+        assert my_stack.getSize() == 1
+
+    def test_verify_pop(self):
+        my_stack = stack()
+        item = "dummy string"
+        my_stack.push(item)
+        assert my_stack.pop() == item
+
+    def test_verify_pop_size(self):
+        my_stack = stack()
+        item = "dummy string"
+        my_stack.push(item)
+        my_stack.pop()
+        assert my_stack.getSize() == 0
+
+    # Tests after this point didn't change code
+    def test_verify_multiple_pop(self):
+        my_stack = stack()
+        item1 = "dummy string 1"
+        item2 = "dummy string 2"
+        my_stack.push(item1)
+        my_stack.push(item2)
+        assert my_stack.pop() == item2
+        assert my_stack.pop() == item1
+
+    def test_verify_stack_does_not_overflow(self):
+        my_stack = stack()
+        item1 = "dummy string 1"
+        item2 = "dummy string 2"
+        item3 = "dummy string 3"
+        item4 = "dummy string 4"
+        my_stack.push(item1)
+        my_stack.push(item2)
+        my_stack.push(item3)
+        my_stack.push(item4)
+        assert my_stack.pop() == item4
+        assert my_stack.pop() == item3
+        assert my_stack.pop() == item2
+        assert my_stack.pop() == item1
+
+    def test_verify_template_type(self):
+        my_stack = stack()
+        item1 = 1234
+        my_stack.push(item1)
+        assert my_stack.pop() == item1
+
