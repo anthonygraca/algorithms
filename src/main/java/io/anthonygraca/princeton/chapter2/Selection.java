382d5119178aecb2d07981938659800e8fedc0a3
matthewgraca <mgraca@cpp.edu>
2021-06-22 14:08:07 -0700

    cleaned up Selection and added algorithm description

--- src/main/java/io/anthonygraca/princeton/chapter2/Selection.java
+++ src/main/java/io/anthonygraca/princeton/chapter2/Selection.java
@@ -1,27 +1,27 @@
 package io.anthonygraca.princeton.chapter2;
 
+/*
+ * Selection works by starting at an index, and scanning the contents to the right of the index
+ *  for the global minimum value. Once the index that contains this value is found, the algorithm
+ *  swaps the contents of the current index and the index with the minimum value.
+ * The result is that everything to the left of the index is sorted. The algorithm then moves on to
+ *  the next index, and scans the right of the container again, continuing in this fashion until
+ *  the end of the container is reached and sorted.
+ */
 public class Selection extends Sort{
   /** 
    * This method implements the selection sort algorithm (n^2)
    * @param a The array being sorted
    */
-  public static void sort(Comparable[] a)
-  {
-    for (int i = 0; i < a.length; i++)
-    {
+  public static void sort(Comparable[] a){
+    for (int i = 0; i < a.length; i++){
       int minIndex = i;
-      // check every index to the right of the current index
-      for (int j = i+1; j < a.length; j++)
-      {
-        // if an item is found to be smaller than the minimum...
-        if (less(a[j], a[minIndex]))
-        { // ...set that index as the new minimum index
+      for (int j = i+1; j < a.length; j++){
+        if (less(a[j], a[minIndex])){ 
           minIndex = j;
         }
       }
-      // when the final, true minIndex is found, exchange contents with i
       exchange(a, i, minIndex);
-      // now, i contains the smallest object everything right of the list
     }
   }
 }
