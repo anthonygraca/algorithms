d203322997b5404c275fb7165264872ca76a7ccf
matthewgraca <mgraca@cpp.edu>
2021-06-23 13:59:48 -0700

    updated Insertion description for clarity

--- src/main/java/io/anthonygraca/princeton/chapter2/Insertion.java
+++ src/main/java/io/anthonygraca/princeton/chapter2/Insertion.java
@@ -1,14 +1,14 @@
 package io.anthonygraca.princeton.chapter2;
 
 /*
- * Instead of finding a global minimum, Insertion works by comparing two items side by side;
+ * Instead of finding a global minimum, Insertion works by comparing two adjacent items;
  *  if the item at the right is smaller, then it swaps places with the item at the left.
  *  After swapping, it compares itself with the item to the left of it. Again, if it is smaller, it
  *  swaps places. This continues until the item to the left of it is smaller.
  *  Then it returns to the index where it initially began, increments, and compares again until that
  *  item is placed properly.
- * In summation, the name of the game is local swapping until the item belongs, relative to the 
- *  items left of the current index.
+ * In summation, the name of the game is local swapping with adjacent items until the item belongs, 
+ *  relative to the items left of the current index.
  */
 public class Insertion extends Sort{
   /**
