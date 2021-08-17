07a85dce2a93832ff1120360fe419a06096fc439
Anthony Graca <anthonygraca@gmail.com>
2021-08-16 22:58:59 -0700

    added code for insertion sort

--- src/main/cpp/sorting/insertion/insertion-sort.h
+++ src/main/cpp/sorting/insertion/insertion-sort.h
@@ -1,13 +1,19 @@
 #ifndef ALGORITHMS_PRINCETON_SORTING_INSERTION_H_
 #define ALGORITHMS_PRINCETON_SORTING_INSERTION_H_
 
-#include <string>
+#include <vector>
 
 namespace algorithms {
 class InsertionSort {
   public:
-    std::string sort(std::string input) {
-      return "abcde";
+    std::vector<int> sort(std::vector<int> input) {
+      int n = input.size();
+      for (int i = 1; i < n; i++) {
+        for (int j = i; j > 0 && input[j] < input[j-1]; j--) {
+          std::swap(input[j], input[j-1]);
+        }
+      }
+      return input;
     }
 };
 } //  namespace algorithms
--- src/test/cpp/sorting/insertion/insertion-sort-test.cpp
+++ src/test/cpp/sorting/insertion/insertion-sort-test.cpp
@@ -3,6 +3,8 @@
 #include "gtest/gtest.h"
 
 TEST(InsertionSort, FirstExample) {
+  std::vector<int> input{2,3,4,1};
   algorithms::InsertionSort algo;
-  ASSERT_EQ("abcde", algo.sort("bcdea"));
+  std::vector<int> expected{1,2,3,4};
+  ASSERT_EQ(expected, algo.sort(input));
 }
