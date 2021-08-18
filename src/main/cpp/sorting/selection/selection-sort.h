7ff78698bda43eed3f6a8abf8d2d23940a5260ca
Anthony Graca <anthonygraca@gmail.com>
2021-08-17 19:22:56 -0700

    implemented selection

--- src/main/cpp/sorting/selection/selection-sort.h
+++ src/main/cpp/sorting/selection/selection-sort.h
@@ -7,7 +7,17 @@ namespace algorithms {
 class SelectionSort {
   public:
     std::vector<int> sort(std::vector<int> input) {
-      return {1,2,3,4};
+      int n = input.size();
+      for (int i = 0; i < n; i++) {
+        int min = i;
+        for (int j = i+1; j < n; j++) {
+          if (input[j] < input[min]) min = j;
+        }
+        int temp = input[i];
+        input[i] = input[min];
+        input[min] = temp;
+      }
+      return input;
     };
 };
 } // namespace algorithms
