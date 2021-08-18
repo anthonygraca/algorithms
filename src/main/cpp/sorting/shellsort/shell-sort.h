ef180780bcbe1fe918a396a93ed75b5ae4821af8
Anthony Graca <anthonygraca@gmail.com>
2021-08-17 22:04:52 -0700

    added implementation of shellsort

--- src/main/cpp/sorting/shellsort/shell-sort.h
+++ src/main/cpp/sorting/shellsort/shell-sort.h
@@ -7,7 +7,21 @@ namespace algorithms {
 class ShellSort {
   public:
     std::vector<int> sort(std::vector<int> input) {
-      return {1,2,3,4};
+      int n = input.size();
+      int h = 1;
+      while (h < n/2) h = 3*h + 1; // 1, 4, 13, 40, 121, 364, 1093, ...
+      while (h >= 1) { // h-sort the array
+        for (int i = h; i < n; i++) { 
+          // Insert input[i] amoung a[i-h], a[i-2*h], a[i-3*h], ... 
+          for (int j = i; j >= h && input[j] < input[j-h]; j -=h) {
+            int temp = input[j];
+            input[j] = input[j-h];
+            input[j-h] = temp;
+          }
+        }
+        h = h/3;
+      }
+      return input;
     }
 };
 } // namespace algorithms
