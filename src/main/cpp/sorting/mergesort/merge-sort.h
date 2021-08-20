3d63d3dc6e7dd2ec428a58bbfd41253bfe29082f
Anthony Graca <anthonygraca@gmail.com>
2021-08-19 22:13:03 -0700

    added top down approach to mergesort

--- src/main/cpp/sorting/mergesort/merge-sort.h
+++ src/main/cpp/sorting/mergesort/merge-sort.h
@@ -6,11 +6,19 @@
 namespace algorithms {
 class MergeSort {
   public:
-    std::vector<int> sort(std::vector<int> input) {
-      return {1,2,3,4};
+    std::vector<int> sort(std::vector<int>& input) {
+      sort(input, 0, input.size()-1);
+      return input;
     }
+    void sort(std::vector<int>& input, int low, int high) {
+      if (high <= low) return;
+      int mid = (low + high) / 2;
+      sort(input, low, mid);
+      sort(input, mid+1, high);
+      merge(input, low, mid, high);
+    }
+    /* MergeSort does not work in-place and requires O(n) space */
     std::vector<int> merge(std::vector<int>& a, int low, int mid, int high) {
-
       int i = low;
       int j = mid+1;
       std::vector<int> new_vector;
@@ -18,7 +26,7 @@ class MergeSort {
         new_vector.push_back(element);
       }
       for (int k = low; k <= high; k++) {
-        if (i < mid) a[k] = new_vector[j++];
+        if (i > mid) a[k] = new_vector[j++];
         else if (j > high) a[k] = new_vector[i++];
         else if (new_vector[j] < new_vector[i]) a[k] = new_vector[j++];
         else a[k] = new_vector[i++];
