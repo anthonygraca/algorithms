bb03e560ed624cbf2a3ef69e123d8b28b49520bc
Anthony Graca <anthonygraca@gmail.com>
2021-08-19 22:35:34 -0700

    added bottom up merge sort

--- src/main/cpp/sorting/mergesort/merge-sort-bottom-up.h
+++ src/main/cpp/sorting/mergesort/merge-sort-bottom-up.h
@@ -7,7 +7,28 @@ namespace algorithms {
 class MergeSortBU {
   public:
     std::vector<int> sort(std::vector<int>& input) {
-      return {1,2,3,4};
+      for (int len = 1; len < input.size(); len *= 2) {
+        for (int low = 0; low < input.size()-len; low += len+len) {
+          merge(input, low, low+len-1, std::min<int>(low+len+len-1, input.size()-1));
+        }
+      }
+      return input;
+    }
+
+    std::vector<int> merge(std::vector<int>& a, int low, int mid, int high) {
+      int i = low;
+      int j = mid+1;
+      std::vector<int> new_vector;
+      for (auto element : a) {
+        new_vector.push_back(element);
+      }
+      for (int k = low; k <= high; k++) {
+        if (i > mid) a[k] = new_vector[j++];
+        else if (j > high) a[k] = new_vector[i++];
+        else if (new_vector[j] < new_vector[i]) a[k] = new_vector[j++];
+        else a[k] = new_vector[i++];
+      }
+      return a;
     }
 };
 } // namespace algorithms
