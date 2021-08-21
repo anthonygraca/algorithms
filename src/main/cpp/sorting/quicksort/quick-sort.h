98367add6c7d4d17803eb7d5fdcc9a8ffec62bc1
Anthony Graca <anthonygraca@gmail.com>
2021-08-20 19:31:17 -0700

    added implementation of quicksort

--- src/main/cpp/sorting/quicksort/quick-sort.h
+++ src/main/cpp/sorting/quicksort/quick-sort.h
@@ -7,8 +7,33 @@ namespace algorithms {
 class QuickSort {
   public:
     std::vector<int> sort(std::vector<int>& input) {
-      return {1,2,3,4};
+      sort(input, 0, input.size()-1);
+      return input;
     };
+
+  private:
+    void sort(std::vector<int>& input, int low, int high) {
+      if (high <= low) return;
+      int j = partition(input, low, high);
+      sort(input, low, j-1);
+      sort(input, j+1, high);
+    }
+    int partition(std::vector<int>& input, int low, int high) {
+      int i = low, j = high + 1;
+      auto partition{input[low]};
+      while (true) {
+        while (input[++i] < partition) if (i == high) break;
+        while (partition < input[--j]) if (j == low) break;
+        if (i >= j) break;
+        auto temp{input[i]};
+        input[i] = input[j];
+        input[j] = temp;
+      }
+      auto temp{input[low]};
+      input[low] = input[j];
+      input[j] = temp;
+      return j;
+    }
 };
 } // namespace algorithms
 
