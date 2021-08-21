62e676253267f5c1910c2d6f4857fa7626680c57
Anthony Graca <anthonygraca@gmail.com>
2021-08-20 19:47:54 -0700

    added some implementation for heapsort

--- src/main/cpp/sorting/heapsort/heap-sort.h
+++ src/main/cpp/sorting/heapsort/heap-sort.h
@@ -7,6 +7,19 @@ namespace algorithms {
 class HeapSort {
   public:
     std::vector<int> sort(std::vector<int>& input) {
+      /*
+      int n = input.size();
+      for (int k = n/2; k >= 1; k--) {
+        //sink(input, k, n);
+      }
+      while (n > 1) {
+        auto temp{input[1]};
+        input[1] = input[n];
+        input[n--] = temp;
+        //sink(input, 1, n);
+      }
+      */
+    
       return {1,2,3,4};
     }
 
