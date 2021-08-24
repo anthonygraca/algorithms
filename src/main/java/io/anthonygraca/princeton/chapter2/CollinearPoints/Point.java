f2eb3281ffd8c127ee5b316a546a267c8ac9d6dd
Anthony Graca <anthonygraca@gmail.com>
2021-08-23 17:27:12 -0700

    implemented compareTo() for Point class

--- src/main/java/io/anthonygraca/princeton/chapter2/CollinearPoints/Point.java
+++ src/main/java/io/anthonygraca/princeton/chapter2/CollinearPoints/Point.java
@@ -77,10 +77,13 @@ public class Point implements Comparable<Point> {
      *         argument point
      */
     public int compareTo(Point that) {
-      if (this.x < that.x && this.y < that.y) return -1;
-      if (this.x > that.x && this.y > that.y) return 1;
+      if (this.y < that.y) return -1;
+      if (this.y > that.y) return 1;
+      if (this.y == that.y) {
+        if (this.x < that.x) return -1;
+        if (this.x > that.x) return 1;
+      }
       return 0;
-        /* YOUR CODE HERE */
     }
 
     /**
--- src/test/java/io/anthonygraca/princeton/chapter2/CollinearPoints/PointTest.java
+++ src/test/java/io/anthonygraca/princeton/chapter2/CollinearPoints/PointTest.java
@@ -4,11 +4,25 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class PointTest {
+public class ComparePointTest {
   @Test
   public void compareStrictlyLessThan() {
     Point pointA = new Point(0,0);
     Point pointB = new Point(1,1);
+    assertTrue(pointA.compareTo(pointB) < 0);
+  }
+
+  @Test
+  public void compareLessThanOnlyY() {
+    Point pointA = new Point(0,0);
+    Point pointB = new Point(0,1);
+    assertTrue(pointA.compareTo(pointB) < 0);
+  }
+
+  @Test
+  public void compareLessThanOnlyYGreaterX() {
+    Point pointA = new Point(1,0);
+    Point pointB = new Point(0,1);
     assertTrue(pointA.compareTo(pointB) < 0);
   }
 
@@ -20,9 +34,37 @@ public class PointTest {
   }
 
   @Test
+  public void compareEqualYGreaterX() {
+    Point pointA = new Point(1,0);
+    Point pointB = new Point(0,0);
+    assertTrue(pointA.compareTo(pointB) > 0);
+  }
+
+  @Test
+  public void compareEqualYLessX() {
+    Point pointA = new Point(0,0);
+    Point pointB = new Point(1,0);
+    assertTrue(pointA.compareTo(pointB) < 0);
+  }
+
+  @Test
   public void compareStrictlyGreaterThan() {
     Point pointA = new Point(1,1);
     Point pointB = new Point(0,0);
+    assertTrue(pointA.compareTo(pointB) > 0);
+  }
+
+  @Test
+  public void compareGreaterYEqualX() {
+    Point pointA = new Point(1,1);
+    Point pointB = new Point(1,0);
+    assertTrue(pointA.compareTo(pointB) > 0);
+  }
+
+  @Test
+  public void compareGreaterYLeserX() {
+    Point pointA = new Point(0,1);
+    Point pointB = new Point(1,0);
     assertTrue(pointA.compareTo(pointB) > 0);
   }
 }
