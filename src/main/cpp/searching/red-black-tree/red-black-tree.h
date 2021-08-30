1fd9b85598ebee9d76de5d0fbdb59adfb4e41547
Anthony Graca <anthonygraca@gmail.com>
2021-08-29 18:21:01 -0700

    duplicated code from bst to rb tree

--- src/main/cpp/searching/red-black-tree/red-black-tree.h
+++ src/main/cpp/searching/red-black-tree/red-black-tree.h
@@ -6,17 +6,188 @@
 namespace algorithms {
 class RedBlackTree {
   public:
-    int size() {
-      return 1;
+    RedBlackTree() = default;
+    ~RedBlackTree() {
+      postorder_delete(m_root);
     }
-    std::string inorder() {
-      return "S";
-    }
+    RedBlackTree(RedBlackTree&&) = delete;
+    RedBlackTree& operator=(RedBlackTree&&) = delete;
+    RedBlackTree(const RedBlackTree&) = delete;
+    RedBlackTree& operator=(const RedBlackTree&) = delete;
     void put(char key, int val) {
+      m_root = put(m_root, key, val);
     }
     int get(char key) {
-      return 0;
+      return get(m_root, key);
     }
+    std::string inorder() {
+      return inorder(m_root);
+    }
+    int size() {
+      return size(m_root);
+    }
+    bool isEmpty() {
+      return size() == 0;
+    }
+    char min() {
+      if (isEmpty()) return -1;
+      Node* x = min(m_root);
+      return x->m_key;
+    }
+    void deleteMin() {
+      if (!isEmpty()) {
+        m_root = deleteMin(m_root);
+      }
+    }
+    void deleteNode(char key) {
+      m_root = deleteNode(m_root, key);
+    }
+    char floor(char key) {
+      Node* x = floor(m_root, key);
+      if (x == nullptr) throw new std::runtime_error("No such element");
+      return x->m_key;
+    }
+    char select(int k) {
+      if (k < 0 || k >= size()) throw new std::invalid_argument("illegal argument");
+      Node* x = select(m_root, k);
+      return x->m_key;
+    }
+    int rank(char key) {
+      return rank(key, m_root);
+    }
+  private:
+    struct Node {
+      char m_key;
+      int m_val;
+      Node* m_left = nullptr;
+      Node* m_right = nullptr;
+      int m_nodes_in_subtree;
+      Node(char key, int val, int n) 
+        : m_key(key), m_val(val), m_nodes_in_subtree(n) {};
+    };
+    /* Change key's value to val if key in subtree rooted at x.
+     * Otherwise, add new node to subtree associating key with val. */
+    Node* put(Node* x, char key, int val) {
+      if (x == nullptr) return new Node(key, val, 1);
+      if (key < x->m_key) {
+        x->m_left = put(x->m_left, key, val);
+      }
+      else if (key > x->m_key) {
+        x->m_right = put(x->m_right, key, val);
+      }
+      else {
+        x->m_val = val;
+      }
+      x->m_nodes_in_subtree = size(x->m_left) + size(x->m_right) + 1;
+      return x;
+    }
+    /* Return value associated with key in the subtree rooted at x
+     * Return -1 if key not present in subtree rooted at x. */
+    int get(Node* x, int key) {
+      if (x != nullptr) {
+        if (key < x->m_key) {
+          return get(x->m_left, key);
+        }
+        else if (key > x->m_key) {
+          return get(x->m_right, key);
+        }
+        else {
+          return x->m_val;
+        }
+      }
+      return -1;
+    }
+    int size(Node* x) {
+      if (x == nullptr) return 0;
+      return x->m_nodes_in_subtree;
+    }
+    Node* min(Node* x) {
+      if (x->m_left == nullptr) return x;
+      return min(x->m_left);
+    }
+    Node* deleteMin(Node* x) {
+      if (x->m_left == nullptr) {
+        Node* save_right_child = x->m_right;
+        delete x;
+        return save_right_child;
+      }
+      x->m_left = deleteMin(x->m_left);
+      x->m_nodes_in_subtree = size(x->m_left) + size(x->m_right) + 1;
+      return x;
+    }
+    /*
+     * 1.) Save a link to the node to be deleted in, t, tree
+     * 2.) Set node to be deleted, x, to point to its successor min(t->m_right)
+     * 3.) Set the right link of x (which contains all keys larger than 
+     *     x->m_key) to detachMin(t->m_right), the link to the BST containing
+     *     all the keys that are larger than x->m_key after the deletion.
+     * 4.) Set the left link of x (which was null) to t->m_left (all the keys
+     *     that are less than both the deleted key and its successor)
+     */
+    Node* deleteNode(Node* x, char key) {
+      if (x == nullptr) return nullptr; 
+      if (key < x->m_key) x->m_left = deleteNode(x->m_left, key);
+      else if (key > x->m_key) x->m_right = deleteNode(x->m_right, key);
+      else {
+        if (x->m_right == nullptr) return x->m_left;
+        if (x->m_left == nullptr) return x->m_right;
+        Node* t = x;
+        x = min(t->m_right);
+        x->m_right = detachMin(t->m_right);
+        x->m_left = t->m_left;
+        delete t;
+      }
+      x->m_nodes_in_subtree = size(x->m_left) + size(x->m_right) + 1;
+      return x;
+    }
+    std::string inorder(Node* x) {
+      std::string visited{""};
+      if (x != nullptr) {
+        visited += inorder(x->m_left);
+        visited += x->m_key;
+        visited += inorder(x->m_right);
+      }
+      return visited;
+    }
+    void postorder_delete(Node* x) {
+      if (x != nullptr) {
+        postorder_delete(x->m_left);
+        postorder_delete(x->m_right);
+        delete x;
+      }
+    }
+    Node* detachMin(Node* x) {
+      if (x->m_left == nullptr) {
+        return x->m_right;
+      }
+      x->m_left = detachMin(x->m_left);
+      x->m_nodes_in_subtree = size(x->m_left) + size(x->m_right) + 1;
+      return x;
+    }
+    Node* floor(Node* x, char key) {
+      if (x == nullptr) return nullptr;
+      if (key == x->m_key) return x;
+      if (key < x->m_key) return floor(x->m_left, key);
+      Node* t = floor(x->m_right, key);
+      if (t != nullptr) return t;
+      else return x;
+    }
+    Node* select(Node* x, int k) {
+      // Return Node* containing key of rank k
+      if (x == nullptr) return nullptr;
+      int t = size(x->m_left);
+      if (t > k) return select(x->m_left, k);
+      else if (t < k) return select(x->m_right, k-t-1);
+      else return x;
+    }
+    int rank(char key, Node* x) {
+      // Return number of keys less than ke in the subtree rooted at x
+      if (x == nullptr) return 0;
+      if (key < x->m_key) return rank(key, x->m_left);
+      if (key > x->m_key) return 1 + size(x->m_left) + rank(key, x->m_left);
+      else return size(x->m_left);
+    }
+    Node* m_root = nullptr;
 };
 } // namespace algorithms
 
--- src/test/cpp/searching/red-black-tree/red-black-tree-test.cpp
+++ src/test/cpp/searching/red-black-tree/red-black-tree-test.cpp
@@ -1,5 +1,7 @@
 #include "searching/red-black-tree/red-black-tree.h"
 
+#include <type_traits>
+
 #include "gtest/gtest.h"
 
 TEST(RedBlackTree, TrivialExample) {
@@ -9,4 +11,190 @@ TEST(RedBlackTree, TrivialExample) {
   ASSERT_EQ(1, bst.size());
   ASSERT_EQ("S", bst.inorder());
   ASSERT_EQ(0, bst.get(input[0]));
+}
+
+TEST(RedBlackTree, TwoElements) {
+  constexpr int insert_count{2};
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < insert_count) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ(val, bst.size());
+  ASSERT_EQ("ES", bst.inorder());
+  ASSERT_EQ(val-1, bst.get(input[val-1]));
+}
+
+TEST(RedBlackTree, ThreeElements) {
+  constexpr int insert_count{3};
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < insert_count) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ(val, bst.size());
+  ASSERT_EQ("AES", bst.inorder());
+  ASSERT_EQ(val-1, bst.get(input[val-1]));
+}
+
+TEST(RedBlackTree, FourElements) {
+  constexpr int insert_count{4};
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < insert_count) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ(val, bst.size());
+  ASSERT_EQ("AERS", bst.inorder());
+  ASSERT_EQ(val-1, bst.get(input[val-1]));
+}
+
+TEST(RedBlackTree, InsertWholeExample) {
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < input.size()) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ(10, bst.size());
+  ASSERT_EQ("ACEHLMPRSX", bst.inorder());
+  ASSERT_EQ(val-1, bst.get(input[val-1]));
+}
+
+TEST(RedBlackTree, MinimumValue) {
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < input.size()) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ('A', bst.min());
+}
+
+TEST(RedBlackTree, DeleteMin) {
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < input.size()) {
+    bst.put(input[val], val);
+    val++;
+  }
+  bst.deleteMin();
+  ASSERT_EQ(9, bst.size());
+  ASSERT_EQ("CEHLMPRSX", bst.inorder());
+}
+
+TEST(RedBlackTree, DeleteNodeWithTwoChildren) {
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < input.size()) {
+    bst.put(input[val], val);
+    val++;
+  }
+  bst.deleteNode('E');
+  ASSERT_EQ(9, bst.size());
+  ASSERT_EQ("ACHLMPRSX", bst.inorder());
+}
+
+TEST(RedBlackTree, FloorOfKeyDShouldBeC) {
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < input.size()) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ('C', bst.floor('D'));
+}
+
+TEST(RedBlackTree, FloorOfKeyCShouldBeC) {
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < input.size()) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ('C', bst.floor('C'));
+}
+
+TEST(RedBlackTree, NumberOfNodesSmallerThanLIs4) {
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < input.size()) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ('L', bst.select(4));
+}
+
+TEST(RedBlackTree, RankOfLIs4) {
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < input.size()) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ(4, bst.rank('L'));
+}
+
+TEST(RedBlackTree, RankAndSelectAreInverseOperations) {
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < input.size()) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ('L', bst.select(bst.rank('L')));
+}
+
+TEST(RedBlackTree, SelectAndRankAreInverseOperations) {
+  const std::string input{"SEARCHEXAMPLE"};
+  algorithms::RedBlackTree bst;
+  int val = 0;
+  while (val < input.size()) {
+    bst.put(input[val], val);
+    val++;
+  }
+  ASSERT_EQ(4, bst.rank(bst.select(4)));
+}
+
+TEST(RedBlackTree, IsConstructable) {
+  ASSERT_TRUE(std::is_default_constructible<algorithms::RedBlackTree>::value);
+  // Node Member variable m_root is not trivially default constructable;
+  ASSERT_FALSE(std::is_trivially_default_constructible<algorithms::RedBlackTree>::value);
+  ASSERT_TRUE(std::is_nothrow_default_constructible<algorithms::RedBlackTree>::value);
+}
+
+TEST(RedBlackTree, IsDestructable) {
+  ASSERT_TRUE(std::is_destructible<algorithms::RedBlackTree>::value);
+  // Node Member variable m_root is not trivially default destructible;
+  ASSERT_FALSE(std::is_trivially_destructible<algorithms::RedBlackTree>::value);
+  ASSERT_TRUE(std::is_nothrow_destructible<algorithms::RedBlackTree>::value);
+}
+
+TEST(RedBlackTree, IsNotCopyable) {
+  ASSERT_FALSE(std::is_copy_assignable<algorithms::RedBlackTree>::value);
+  ASSERT_FALSE(std::is_copy_constructible<algorithms::RedBlackTree>::value);
+  ASSERT_FALSE(std::is_trivially_copy_constructible<algorithms::RedBlackTree>::value);
+  ASSERT_FALSE(std::is_nothrow_copy_constructible<algorithms::RedBlackTree>::value);
+}
+
+TEST(RedBlackTree, IsNotMovable) {
+  ASSERT_FALSE(std::is_move_assignable<algorithms::RedBlackTree>::value);
+  ASSERT_FALSE(std::is_move_constructible<algorithms::RedBlackTree>::value);
+  ASSERT_FALSE(std::is_trivially_move_constructible<algorithms::RedBlackTree>::value);
+  ASSERT_FALSE(std::is_nothrow_move_constructible<algorithms::RedBlackTree>::value);
 }
