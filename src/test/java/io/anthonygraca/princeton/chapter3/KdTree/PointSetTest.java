21697befcf290ec588e093d0d1d73365ae381262
Anthony Graca <anthonygraca@gmail.com>
2021-09-18 20:22:04 -0700

    wrote trivial test for size()

--- src/test/java/io/anthonygraca/princeton/chapter3/KdTree/PointSetTest.java
+++ src/test/java/io/anthonygraca/princeton/chapter3/KdTree/PointSetTest.java
@@ -1,6 +1,7 @@
 package io.anthonygraca.princeton.chapter2.KdTree;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -9,5 +10,11 @@ public class PointSetTest {
   public void constructedObjectIsEmpty() {
     PointSET set = new PointSET();
     assertTrue(set.isEmpty());
+  }
+
+  @Test
+  public void constructedObjectSizeIsZero() {
+    PointSET set = new PointSET();
+    assertEquals(0, set.size());
   }
 }
