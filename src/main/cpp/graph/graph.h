6ddf4fd917bd04ab166203d1a1b232b06b353c3e
Anthony Graca <anthonygraca@gmail.com>
2021-09-26 14:41:14 -0700

    implemented iterating adjacent vertices

--- src/main/cpp/graph/graph.h
+++ src/main/cpp/graph/graph.h
@@ -1,6 +1,7 @@
 #ifndef ALGORITHMS_PRINCETON_GRAPHS_H_
 #define ALGORITHMS_PRINCETON_GRAPHS_H_
 
+#include <unordered_set>
 #include <vector>
 
 namespace algorithms {
@@ -9,24 +10,24 @@ class Graph {
     Graph() = delete;
     Graph(int v) : m_vertices(v) {
       for(int i = 0; i < v; i++) {
-        std::vector<int> stack;
+        std::unordered_set<int> stack;
         m_adjacency_list.push_back(stack);
       }
     };
     int vertices() {return m_vertices;};
     int edges() {return m_edges;};
     void addEdge(int v, int w) {
-      m_adjacency_list[v].push_back(w);
-      m_adjacency_list[w].push_back(v);
+      m_adjacency_list[v].insert(w);
+      m_adjacency_list[w].insert(v);
       m_edges++;
     }
-    std::vector<int> adj(int v) {
+    std::unordered_set<int> adj(int v) {
       return m_adjacency_list[v];
     }
   private:
     int m_vertices = 0;
     int m_edges = 0;
-    std::vector<std::vector<int>> m_adjacency_list;
+    std::vector<std::unordered_set<int>> m_adjacency_list;
 };
 } // namespace algorithms
 
--- src/test/cpp/graph/graph-test.cpp
+++ src/test/cpp/graph/graph-test.cpp
@@ -1,5 +1,7 @@
 #include "graph/graph.h"
 
+#include <unordered_set>
+
 #include "gtest/gtest.h"
 
 TEST(Graph, NoVertices) {
@@ -27,4 +29,73 @@ TEST(Graph, AddEdge) {
   g.addEdge(0,1);
   ASSERT_EQ(2, g.vertices());
   ASSERT_EQ(1, g.edges());
+}
+
+TEST(Graph, BigExample) {
+  algorithms::Graph g{13};
+  g.addEdge(0,5);
+  g.addEdge(4,3);
+  g.addEdge(0,1);
+  g.addEdge(9,12);
+  g.addEdge(6,4);
+  g.addEdge(5,4);
+  g.addEdge(0,2);
+  g.addEdge(11,12);
+  g.addEdge(9,10);
+  g.addEdge(0,6);
+  g.addEdge(7,8);
+  g.addEdge(9,11);
+  g.addEdge(5,3);
+  ASSERT_EQ(13, g.vertices());
+  ASSERT_EQ(13, g.edges());
+  ASSERT_TRUE(g.adj(0).contains(5));
+  ASSERT_TRUE(g.adj(0).contains(1));
+  ASSERT_TRUE(g.adj(0).contains(2));
+  ASSERT_TRUE(g.adj(0).contains(6));
+  ASSERT_TRUE(g.adj(1).contains(0));
+  ASSERT_TRUE(g.adj(2).contains(0));
+  ASSERT_TRUE(g.adj(3).contains(5));
+  ASSERT_TRUE(g.adj(3).contains(4));
+  ASSERT_TRUE(g.adj(4).contains(5));
+  ASSERT_TRUE(g.adj(4).contains(6));
+  ASSERT_TRUE(g.adj(4).contains(3));
+  ASSERT_TRUE(g.adj(5).contains(3));
+  ASSERT_TRUE(g.adj(5).contains(4));
+  ASSERT_TRUE(g.adj(5).contains(0));
+  ASSERT_TRUE(g.adj(6).contains(0));
+  ASSERT_TRUE(g.adj(6).contains(4));
+  ASSERT_TRUE(g.adj(7).contains(8));
+  ASSERT_TRUE(g.adj(8).contains(7));
+  ASSERT_TRUE(g.adj(9).contains(11));
+  ASSERT_TRUE(g.adj(9).contains(10));
+  ASSERT_TRUE(g.adj(9).contains(12));
+  ASSERT_TRUE(g.adj(10).contains(9));
+  ASSERT_TRUE(g.adj(11).contains(12));
+  ASSERT_TRUE(g.adj(12).contains(11));
+  ASSERT_TRUE(g.adj(12).contains(9));
+}
+
+TEST(Graph, BigExampleAdjacentToZero) {
+  std::unordered_set<int> set;
+  set.insert(6);
+  set.insert(5);
+  set.insert(2);
+  set.insert(1);
+  algorithms::Graph g{13};
+  g.addEdge(0,5);
+  g.addEdge(4,3);
+  g.addEdge(0,1);
+  g.addEdge(9,12);
+  g.addEdge(6,4);
+  g.addEdge(5,4);
+  g.addEdge(0,2);
+  g.addEdge(11,12);
+  g.addEdge(9,10);
+  g.addEdge(0,6);
+  g.addEdge(7,8);
+  g.addEdge(9,11);
+  g.addEdge(5,3);
+  for(int w : g.adj(0)) {
+    ASSERT_TRUE(set.contains(w));
+  }
 }
