7a881940357d91a5fbdc5cdbc402a76809c9f8f7
Anthony Graca <anthonygraca@gmail.com>
2021-09-26 14:23:26 -0700

    implemented graph data structure

--- src/main/cpp/graph/graph.h
+++ src/main/cpp/graph/graph.h
@@ -1,13 +1,32 @@
 #ifndef ALGORITHMS_PRINCETON_GRAPHS_H_
 #define ALGORITHMS_PRINCETON_GRAPHS_H_
 
+#include <vector>
+
 namespace algorithms {
 class Graph {
   public:
-    Graph(int v) : m_vertices(v) {};
+    Graph() = delete;
+    Graph(int v) : m_vertices(v) {
+      for(int i = 0; i < v; i++) {
+        std::vector<int> stack;
+        m_adjacency_list.push_back(stack);
+      }
+    };
     int vertices() {return m_vertices;};
+    int edges() {return m_edges;};
+    void addEdge(int v, int w) {
+      m_adjacency_list[v].push_back(w);
+      m_adjacency_list[w].push_back(v);
+      m_edges++;
+    }
+    std::vector<int> adj(int v) {
+      return m_adjacency_list[v];
+    }
   private:
-    int m_vertices = -1;
+    int m_vertices = 0;
+    int m_edges = 0;
+    std::vector<std::vector<int>> m_adjacency_list;
 };
 } // namespace algorithms
 
--- src/test/cpp/graph/graph-test.cpp
+++ src/test/cpp/graph/graph-test.cpp
@@ -2,7 +2,29 @@
 
 #include "gtest/gtest.h"
 
+TEST(Graph, NoVertices) {
+  algorithms::Graph g{0};
+  ASSERT_EQ(0, g.vertices());
+}
+
+TEST(Graph, NoVerticesNoEdges) {
+  algorithms::Graph g{0};
+  ASSERT_EQ(0, g.vertices());
+}
+
 TEST(Graph, OnlyOneVertex) {
   algorithms::Graph g{1};
   ASSERT_EQ(1, g.vertices());
+}
+
+TEST(Graph, OnlyOneVertexNoEdges) {
+  algorithms::Graph g{1};
+  ASSERT_EQ(0, g.edges());
+}
+
+TEST(Graph, AddEdge) {
+  algorithms::Graph g{2};
+  g.addEdge(0,1);
+  ASSERT_EQ(2, g.vertices());
+  ASSERT_EQ(1, g.edges());
 }
