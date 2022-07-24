1a3803a9bfc81a9ac9f5934b65c4745ae80884bf
anthonygraca <anthonygraca@gmail.com>
2022-07-24 13:22:47 -0700

    handled all corner cases for seam carver

--- src/main/java/io/anthonygraca/princeton/chapter4/SeamCarver/SeamCarver.java
+++ src/main/java/io/anthonygraca/princeton/chapter4/SeamCarver/SeamCarver.java
@@ -3,9 +3,63 @@ package io.anthonygraca.princeton.chapter4.SeamCarver;
 import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
-  SeamCarver(Picture picture) {
+  private final Picture picture_;
+  private int width_;
+  private int height_;
+  public SeamCarver(Picture picture) {
     if (picture == null) {
       throw new IllegalArgumentException("Input is null");
     }
+    picture_ = new Picture(picture);
+    width_ = picture_.width();
+    height_ = picture_.height();
   }
+
+  public Picture picture() {
+    return picture_;
+  }
+
+  public int width() {
+    return picture_.width();
+  }
+  
+  public int height() {
+    return picture_.height();
+  }
+
+  public double energy(int x, int y) {
+    if (x < 0 || x >= picture_.width() || y < 0 || y >= picture_.height()) {
+      throw new IllegalArgumentException("resolution out of bounds");
+    }
+    return 0.0;
+  }
+
+  public void removeHorizontalSeam(int[] seam) {
+    final boolean is_valid = isValidSeam(seam, width_, height_) && height_ > 1;
+    if (!is_valid) {
+      throw new IllegalArgumentException("resolution out of bounds");
+    }
+  }
+  
+  public void removeVerticalSeam(int[] seam) {
+    final boolean is_valid = isValidSeam(seam, height_, width_) && width_ > 1;
+    if (!is_valid) {
+      throw new IllegalArgumentException("resolution out of bounds");
+    }
+  }
+
+  private boolean isValidSeam(int[] seam, int length, int range) {
+    if (seam == null || seam.length != length) {
+      return false;
+    }
+    int previous_entry = seam[0];
+    for (int entry : seam) {
+      if (entry < 0 || entry >= range || Math.abs(previous_entry - entry) > 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+
 }
--- src/test/java/io/anthonygraca/princeton/chapter4/SeamCarver/SeamCarverTest.java
+++ src/test/java/io/anthonygraca/princeton/chapter4/SeamCarver/SeamCarverTest.java
@@ -1,10 +1,135 @@
 package io.anthonygraca.princeton.chapter4.SeamCarver;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarverTest {
   @Test(expected=IllegalArgumentException.class)
   public void CatchNullInputInConstructor() {
     SeamCarver sc = new SeamCarver(null);
   }
+
+  @Test
+  public void WidthOfValidPicture() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    assertEquals(1920, sc.width());
+  }
+
+  @Test
+  public void HeightOfValidPicture() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    assertEquals(1080, sc.height());
+  }
+
+  @Test
+  public void ReturnPicture() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    assertEquals(p, sc.picture());
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void ThrowOnInvalidEnergyWidthLowerBound() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    sc.energy(-1, 0);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void ThrowOnInvalidEnergyWidthUpperBound() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    sc.energy(1920, 0);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void ThrowOnInvalidEnergyHeightLowerBound() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    sc.energy(0, -1);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void ThrowOnInvalidEnergyHeightUpperBound() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    sc.energy(0, 1080);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void RemoveHorizontalSeamThrowsOnNullInput() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    sc.removeHorizontalSeam(null);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void RemoveVerticalSeamThrowsOnNullInput() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    sc.removeVerticalSeam(null);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void RemoveVerticalSeamThrowsOnSmallPicture() {
+    Picture p = new Picture(1, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    int[] seam = new int[1080];
+    sc.removeVerticalSeam(seam);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void RemoveHorizontalSeamThrowsOnSmallPicture() {
+    Picture p = new Picture(1920, 1);
+    SeamCarver sc = new SeamCarver(p);
+    int[] seam = new int[1920];
+    sc.removeHorizontalSeam(seam);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void RemoveVerticalSeamThrowsOnWrongInputLength() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    int[] seam = new int[1089];
+    sc.removeVerticalSeam(seam);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void RemoveHorizontalSeamThrowsOnWrongInputLength() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    int[] seam = new int[1919];
+    sc.removeHorizontalSeam(seam);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void RemoveHorizontalSeamThrowsOnInputOutsideRange() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    int[] seam = new int[1920];
+    seam[0] = 1080;
+    sc.removeHorizontalSeam(seam);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void RemoveHorizontalSeamThrowsOnInputEntriesDifferMoreThanOne() {
+    Picture p = new Picture(1920, 1080);
+    SeamCarver sc = new SeamCarver(p);
+    int[] seam = new int[1920];
+    seam[0] = 1000;
+    seam[1] = 1002;
+    sc.removeHorizontalSeam(seam);
+  }
+
+
+
+  // height and width changes when seam is removed
+
 }
