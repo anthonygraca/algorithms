bcd06d777a2f57498d67146d6893cc1612d574f7
anthonygraca <anthonygraca@gmail.com>
2022-08-04 00:04:41 -0700

    solved leetcode 128 and 36

---
+++ src/main/cpp/leetcode/128-longest-consecutive-sequence.h
@@ -0,0 +1,39 @@
+#ifndef LEETCODE_MEDIUM_ARRAYS_128_LONGEST_CONSECUTIVE_SEQUENCE_H_
+#define LEETCODE_MEDIUM_ARRAYS_128_LONGEST_CONSECUTIVE_SEQUENCE_H_
+
+#include <vector>
+#include <unordered_set>
+
+// Time: O(n)
+// Space: O(n)
+// rip. couldn't solve
+
+namespace leetcode {
+class Solution {
+ public:
+  
+  int LongestConsecutiveSequence(std::vector<int>& nums) {
+    std::unordered_set<int> vertices;
+    for (auto num : nums) {
+      vertices.insert(num);
+    }
+    int length = 0;
+    for (auto num : nums) {
+      if (vertices.find(num-1) == vertices.end()) {
+        int i = num+1;
+        while (vertices.find(i) != vertices.end()) {
+          i++;
+        }
+        length = std::max(length, i-num);
+      }
+    }
+    return length;
+  }
+ private:
+  std::vector<int> vertices;
+  
+};
+
+} // namespace leetcode
+
+#endif
