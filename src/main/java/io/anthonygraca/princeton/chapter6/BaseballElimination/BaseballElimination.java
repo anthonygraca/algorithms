ec123d77d970c3534d748794159d0d333d386b34
Anthony Graca <anthonygraca@gmail.com>
2022-09-17 16:02:31 -0700

    simplified data representation

--- src/main/java/io/anthonygraca/princeton/chapter6/BaseballElimination/BaseballElimination.java
+++ src/main/java/io/anthonygraca/princeton/chapter6/BaseballElimination/BaseballElimination.java
@@ -2,46 +2,87 @@ package io.anthonygraca.princeton.chapter6.BaseballElimination;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import edu.princeton.cs.algs4.In;
 
 public class BaseballElimination {
-  private final int team_count_;
-  private final ArrayList<String> teams_;
+  private ArrayList<String> teams_ = new ArrayList<String>();
+  private HashMap<String, Integer> team_number_ = new HashMap<String, Integer>();
+  private HashMap<String, ArrayList<Integer>> team_data_ =
+      new HashMap<String, ArrayList<Integer>>();
   public BaseballElimination(String filename) {
     if (filename == null) {
       throw new IllegalArgumentException("Input cannot be null");
     }
     try {
       File file = new File(filename);
-      In in = new In(file);
-      if (in.hasNextLine()) {
-        team_count_ = in.readInt();
-      }
-      else {
-        team_count_ = -1;
-      }
-      teams_ = new ArrayList<String>();
-      if (in.hasNextLine()) {
-        String line = in.readLine();
-        if (line.indexOf(' ') != -1) {
-          line.substring(0, line.indexOf(' '));
-        }
-        //Teams_.add(line.substring(0, "atlanta ".indexOf(' ')));
-      }
-      
+      readAndStoreDataFromFile(file);
     } catch (IllegalArgumentException e) {
       throw e;
     }
   }
 
+  private void readAndStoreDataFromFile(File file) {
+    In in = new In(file);
+    readNumberOfTeams(in);
+    readAndStoreTeamData(in);
+  }
+  
+  private int readNumberOfTeams(In in) {
+    int team_count = 0;
+    if (in.hasNextLine()) {
+      team_count = in.readInt();
+      in.readLine(); // consume newline;
+    }
+    return team_count;
+  }
+  
+  private void readAndStoreTeamData(In in) {
+    int team_number = 0;
+    while (in.hasNextLine()) {
+      String line = in.readLine();
+      String[] data = line.split("\\s+");
+      String team_name = data[0];
+      team_number_.put(team_name, team_number);
+      teams_.add(team_name);
+      ArrayList<Integer> team_data = new ArrayList<Integer>();
+      for (int i = 1; i < data.length; i++) {
+        team_data.add(Integer.parseInt(data[i]));
+      }
+      team_data_.put(team_name, team_data);
+      team_number++;
+    }
+  }
+
   public int numberOfTeams() {
-    return team_count_;
+    return team_number_.size();
   }
 
   public Iterable<String> teams() {
     return teams_;
+  }
+
+  public int wins(String team) {
+    final int offset = 0;
+    return team_data_.get(team).get(offset);
+  }
+
+  public int losses(String team) {
+    final int offset = 1;
+    return team_data_.get(team).get(offset);
+  }
+
+  public int remaining(String team) {
+    final int offset = 2;
+    return team_data_.get(team).get(offset);
+  }
+
+  public int against(String a, String b) {
+    final int offset = 3;
+    final int team_number = team_number_.get(b) + offset;
+    return team_data_.get(a).get(team_number);
   }
   
 }
--- src/test/java/io/anthonygraca/princeton/chapter6/BaseballElimination/BaseballEliminationTest.java
+++ src/test/java/io/anthonygraca/princeton/chapter6/BaseballElimination/BaseballEliminationTest.java
@@ -37,14 +37,12 @@ public class BaseballEliminationTest {
     assertEquals(be.numberOfTeams(), 5);
   }
 
-  //@Test
-  public void ValidateIterable() {
+  @Test
+  public void ValidateIterableContainsValues() {
     BaseballElimination be = new BaseballElimination(prefix + "teams4.txt");
     Iterable<String> iterable = be.teams();
     Iterator<String> iterator = iterable.iterator();
     assertTrue(iterator.hasNext());
-    
-    //assertEquals(be.numberOfTeams(), 4);
   }
 
   @Test
@@ -64,6 +62,83 @@ public class BaseballEliminationTest {
     String team_name = in.readLine();
     assertEquals(team_name.substring(0, team_name.indexOf(' ')), "Atlanta");
   }
-
   
+  @Test
+  public void ValidateIterableExactValues() {
+    BaseballElimination be = new BaseballElimination(prefix + "teams4.txt");
+    Iterable<String> iterable = be.teams();
+    Iterator<String> iterator = iterable.iterator();
+    assertEquals("Atlanta", iterator.next());
+    assertEquals("Philadelphia", iterator.next());
+    assertEquals("New_York", iterator.next());
+    assertEquals("Montreal", iterator.next());
+    assertFalse(iterator.hasNext());
+  }
+
+  @Test
+  public void GetWinsForAtlanta() {
+    BaseballElimination be = new BaseballElimination(prefix + "teams4.txt");
+    assertEquals(83, be.wins("Atlanta"));
+  }
+
+  @Test
+  public void GetOtherWins() {
+    BaseballElimination be = new BaseballElimination(prefix + "teams4.txt");
+    assertEquals(80, be.wins("Philadelphia"));
+    assertEquals(78, be.wins("New_York"));
+    assertEquals(77, be.wins("Montreal"));
+  }
+
+  @Test
+  public void GetLosses() {
+    BaseballElimination be = new BaseballElimination(prefix + "teams4.txt");
+    assertEquals(71, be.losses("Atlanta"));
+    assertEquals(79, be.losses("Philadelphia"));
+    assertEquals(78, be.losses("New_York"));
+    assertEquals(82, be.losses("Montreal"));
+  }
+  
+  @Test
+  public void GetRemainingGames() {
+    BaseballElimination be = new BaseballElimination(prefix + "teams4.txt");
+    assertEquals(8, be.remaining("Atlanta"));
+    assertEquals(3, be.remaining("Philadelphia"));
+    assertEquals(6, be.remaining("New_York"));
+    assertEquals(3, be.remaining("Montreal"));
+  }
+
+  @Test
+  public void ValidateStringSplitRegex() {
+    String team_data = "Atlanta       83 71  8  0 1 6 1";
+    String[] data = team_data.split("\\s+");
+    assertEquals("Atlanta", data[0]);
+    assertEquals(83, Integer.parseInt(data[1]));
+    assertEquals(71, Integer.parseInt(data[2]));
+    assertEquals(8, Integer.parseInt(data[3]));
+    assertEquals(0, Integer.parseInt(data[4]));
+    assertEquals(1, Integer.parseInt(data[5]));
+    assertEquals(6, Integer.parseInt(data[6]));
+    assertEquals(1, Integer.parseInt(data[7]));
+  }
+  
+  @Test
+  public void GetRemainingGamesAgainstSpecificTeam() {
+    BaseballElimination be = new BaseballElimination(prefix + "teams4.txt");
+    assertEquals(0, be.against("Atlanta", "Atlanta"));
+    assertEquals(1, be.against("Atlanta", "Philadelphia"));
+    assertEquals(6, be.against("Atlanta", "New_York"));
+    assertEquals(1, be.against("Atlanta", "Montreal"));
+    assertEquals(1, be.against("Philadelphia", "Atlanta"));
+    assertEquals(0, be.against("Philadelphia", "Philadelphia"));
+    assertEquals(0, be.against("Philadelphia", "New_York"));
+    assertEquals(2, be.against("Philadelphia", "Montreal"));
+    assertEquals(6, be.against("New_York", "Atlanta"));
+    assertEquals(0, be.against("New_York", "Philadelphia"));
+    assertEquals(0, be.against("New_York", "New_York"));
+    assertEquals(0, be.against("New_York", "Montreal"));
+    assertEquals(1, be.against("Montreal", "Atlanta"));
+    assertEquals(2, be.against("Montreal", "Philadelphia"));
+    assertEquals(0, be.against("Montreal", "New_York"));
+    assertEquals(0, be.against("Montreal", "Montreal"));
+  }  
 }
